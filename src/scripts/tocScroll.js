c040bb915b7ec01fb7f04cd2ee94f43d19487d30
Peng Zhong <peng@nylira.com>
2016-08-29 21:19:21 +0200

    fix table of contents small screen non-scrolling issue

--- src/scripts/tocScroll.js
+++ src/scripts/tocScroll.js
@@ -19,5 +19,20 @@ export default function () {
   })
 
   let toc = document.querySelector('.toc-wrapper')
-  Ps.initialize(toc)
+
+  // disable the scrollbar if the screen is small
+  let viewportWidth = $(window).width()
+  if (viewportWidth >= 1280) {
+    Ps.initialize(toc)
+  }
+
+  // watch window resize
+  $(window).resize(function () {
+    let viewportWidth = $(window).width()
+    if (viewportWidth >= 1280) {
+      Ps.initialize(toc)
+    } else {
+      Ps.destroy(toc)
+    }
+  })
 }
