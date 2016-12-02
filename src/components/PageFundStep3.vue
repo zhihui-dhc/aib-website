303eed8ed1c4305715a3db5faebfe2ce1c2621a7
Peng Zhong <peng@nylira.com>
2016-12-02 15:43:59 +0800

    add steps

---
+++ src/components/PageFundStep3.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="fund-step">
+    <form class="form-default" v-on:submit.prevent="setFundProgress(4)">
+      <div class="form-header">
+        <div class="title">{{ $t('siteFund.stepThree.title') }}</div>
+        <div class="subtitle">{{ $t('siteFund.stepThree.subtitle') }}</div>
+      </div>
+      <div class="form-group">
+        <label>{{ $t('siteFund.stepThree.atomCount') }}</label>
+        <div class="input-group">
+          <input class="highlight-on-focus" type="number" v-model="user.atoms" required min="1" max="1000000">
+          <div class="input-group-addon">Atoms</div>
+        </div>
+      </div>
+      <div class="form-group">
+        <label>{{ $t('siteFund.stepThree.btcPrice') }}</label>
+        <div class="input-group">
+          <input class="highlight-on-focus" type="number" disabled :value="bitcoinPrice">
+          <div class="input-group-addon">BTC</div>
+        </div>
+      </div>
+      <div class="form-footer">
+        <input type="submit" class="btn" :value="$t('siteFund.btnContinue')">
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+// import { required, minLength } from 'vuelidate/lib/validators'
+import { required } from 'vuelidate/lib/validators'
+export default {
+  methods: {
+    setFundProgress (step) { this.step = step }
+  },
+  props: ['user', 'bitcoinPrice'],
+  validations: {
+    user: {
+      atoms: {
+        required
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/PageFundStep4.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="fund-step">
+    <form class="form-default" v-on:submit.prevent="goHome">
+      <div class="form-header">
+        <div class="title">{{ $t('siteFund.stepFour.title') }}</div>
+        <div class="subtitle">{{ $t('siteFund.stepFour.subtitle') }}</div>
+      </div>
+      <div class="form-group">
+        <label>{{ $t('siteFund.stepFour.depositAddress') }}</label>
+        <div class="input-group">
+          <input class="highlight-on-focus" type="text" :value="bitcoinAddress">
+        </div>
+      </div>
+      <div class="form-group">
+        <label>{{ $t('siteFund.stepFour.totalPrice') }}</label>
+        <div class="input-group">
+          <input class="highlight-on-focus" type="number" disabled :value="bitcoinPrice">
+          <div class="input-group-addon">BTC</div>
+        </div>
+      </div>
+      <div class="form-footer">
+        <input type="submit" class="btn" :value="$t('siteFund.btnFinished')">
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      bitcoinAddress: '1NAfx5GEZHR8t69LjxTeShPP4XXaxeUqQw'
+    }
+  },
+  methods: {
+    setFundProgress (step) { this.step = step },
+    goHome () {
+      this.$router.push('/')
+    }
+  },
+  props: ['step', 'user', 'bitcoinPrice']
+}
+</script>
