2bd10fe03601c6aeee80ce5b3a1f965258123c7d
Peng Zhong <peng@nylira.com>
2017-01-11 18:08:03 +0900

    update 404 page

--- src/components/Page404.vue
+++ src/components/Page404.vue
@@ -11,10 +11,10 @@
           <p>The page you visited either does not exist or has been deleted in a website reshuffle. We're sorry for the inconvenience. Try visiting one of these pages below:</p>
           <ul>
             <li><router-link :to="'/'">Home</router-link></li>
-            <li><router-link :to="'/intro'">Intro</router-link></li>
-            <li><router-link :to="'/docs'">Docs</router-link></li>
-            <li><router-link :to="'/community'">Community</router-link></li>
             <li><router-link :to="'/blog'">Blog</router-link></li>
+            <li><router-link :to="'/plan'">Plan</router-link></li>
+            <li><router-link :to="'/faq'">FAQ</router-link></li>
+            <li><router-link :to="'/whitepaper'">Whitepaper</router-link></li>
           </ul>
           <p>Please notify us at <a href="mailto:hello@tendermint.com">hello@tendermint.com</a> and we will fix this error as soon as we can. Thank you for your patience.</p>
         </div>
@@ -27,7 +27,7 @@
 export default {
   name: 'page-404',
   mounted () {
-    document.title = '404 - Tendermint'
+    document.title = '404 - Cosmos - Internet of Blockchains'
   }
 }
 </script>
