1e9a699d22ec574fb71f27f63e5fce929af2ec53
Peng Zhong <peng@nylira.com>
2017-02-10 17:41:47 +0800

    restore session module

--- src/store/modules/session.js
+++ src/store/modules/session.js
@@ -1,3 +1,32 @@
+import firebase from 'firebase'
+let ref
+
+function watchRef () {
+  ref.on('child_added', function (snapshot, prevChildKey) {
+    state.votes[snapshot.key] = snapshot.val()
+    // console.log('votes added:', JSON.stringify(state.votes))
+  })
+  ref.on('child_changed', function (snapshot) {
+    state.votes[snapshot.key] = snapshot.val()
+    // console.log('votes changed:', JSON.stringify(state.votes))
+  })
+  ref.on('child_removed', function (snapshot) {
+    delete state.votes[snapshot.key]
+    // console.log('votes removed:', JSON.stringify(state.votes))
+  })
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  state.votes = {}
+  if (user) {
+    // console.log('user logged in!')
+    ref = firebase.database().ref(`users/${user.uid}/votes`)
+    watchRef()
+  } else {
+    // console.log('user logged out')
+  }
+})
+
 const emptyUser = {
   displayName: '',
   email: '',
@@ -7,7 +36,8 @@ const emptyUser = {
 
 const state = {
   request: '',
-  user: JSON.parse(JSON.stringify(emptyUser))
+  user: JSON.parse(JSON.stringify(emptyUser)),
+  votes: {}
 }
 
 const mutations = {
@@ -22,11 +52,12 @@ const mutations = {
   },
   clearSessionUser (state) {
     state.user = JSON.parse(JSON.stringify(emptyUser))
+    state.votes = {}
     // console.log('setting session user')
   },
   setSessionUserDisplayName (state, value) {
     state.user.displayName = value
-    // console.log('seting vuex user.displayName', value)
+    console.log('seting vuex user.displayName', value)
   },
   setSessionUserEmail (state, value) {
     state.user.email = value
@@ -36,6 +67,16 @@ const mutations = {
   },
   setSessionUserUid (state, value) {
     state.user.uid = value
+  },
+  sessionDownvoteComment (state, commentId) {
+    ref.child(commentId).set(-1)
+  },
+  sessionUpvoteComment (state, commentId) {
+    ref.child(commentId).set(1)
+  },
+  sessionClearVoteComment (state, commentId) {
+    // console.log('clearing vote')
+    ref.child(commentId).set(null)
   }
 }
 
