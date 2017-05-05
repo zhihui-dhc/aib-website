ff369cbd66c619c18965f94b69e2fdaa469f8679
Peng Zhong <peng@nylira.com>
2017-05-05 11:57:41 +0200

    fix percentage scrolling

--- src/scripts/percentageScrolling.js
+++ src/scripts/percentageScrolling.js
@@ -1,32 +1,32 @@
-function scrollListener (amountOfHtmlToScroll, scrollSpeedMult, amountOfSidebarToScroll, toc) {
-  let htmlScrollTop = window.scrollY
-  let ratioScrolled = htmlScrollTop / amountOfHtmlToScroll / scrollSpeedMult
-  let sidebarScrollTop = ratioScrolled * amountOfSidebarToScroll
+let toc, htmlScrollHeight, htmlViewportHeight, amountOfHtmlToScroll, sidebarScrollHeight, sidebarHeight, amountOfSidebarToScroll
 
+function scrollListener () {
+  let htmlScrollTop = window.scrollY
+  let ratioScrolled = htmlScrollTop / amountOfHtmlToScroll // scrollSpeedMult
+  let sidebarScrollTop = ratioScrolled * amountOfSidebarToScroll
   toc.scrollTop = sidebarScrollTop
 }
 
 function scrollIt (scrollSpeedMult = 1.0) {
-  let toc = document.querySelector('.minimal-toc')
+  toc = document.querySelector('.minimal-toc')
 
-  let htmlScrollHeight = document.body.scrollHeight
-  let htmlViewportHeight = window.innerHeight
-  let amountOfHtmlToScroll = htmlScrollHeight - htmlViewportHeight
+  htmlScrollHeight = document.body.scrollHeight
+  htmlViewportHeight = window.innerHeight
+  amountOfHtmlToScroll = htmlScrollHeight - htmlViewportHeight
 
-  let sidebarScrollHeight = document.querySelector('.minimal-toc').scrollHeight
-  let sidebarHeight = document.querySelector('.minimal-toc').offsetHeight
-  let amountOfSidebarToScroll = sidebarScrollHeight - sidebarHeight
+  sidebarScrollHeight = document.querySelector('.minimal-toc').scrollHeight
+  sidebarHeight = document.querySelector('.minimal-toc').offsetHeight
+  amountOfSidebarToScroll = sidebarScrollHeight - sidebarHeight
 
   // console.log('htmlScrollH', htmlScrollHeight, 'htmlViewport', htmlViewportHeight)
   // console.log('sidebarScrollH', sidebarScrollHeight, 'sidebarH', sidebarHeight)
 
-  window.addEventListener('scroll',
-    scrollListener(amountOfHtmlToScroll, scrollSpeedMult, amountOfSidebarToScroll, toc))
+  window.addEventListener('scroll', scrollListener)
 }
 
 export default function () {
   if (document.documentElement.clientWidth >= 1024) {
-    scrollIt(1.0)
+    scrollIt()
   }
 
   // disable percentage scrolling on smaller displays
