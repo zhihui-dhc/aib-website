48d1316a3fa3ca5f3f01cc80f6b5aae2805652a7
Hua-sheng <31681100+huasheng317482454@users.noreply.github.com>
2017-09-20 12:00:18 +0800

    Update CardEvents.vue
    Use flex optimization

--- src/components/CardEvents.vue
+++ src/components/CardEvents.vue
@@ -1,23 +1,18 @@
 <template>
-  <ul class="clearfix card-event">
-    <li v-for="item in events">
-      <div class="content">
-        <div>
-          <div class="img-box">
-            <img :src="'./static/events/'+item.img">
-          </div>
-        </div>
-        <div class="title">{{item.title}}</div>
-        <div class="date">{{item.date}}</div>
-        <div class="details">{{item.details}}</div>
-        <div class="href">
-          <a :href="item.href" target="_blank">Visit event website -></a>
-        </div>
+  <div class="card-events">
+    <div class="flexlist" v-for="item in events">
+      <div class="img-box">
+        <img :src="'./static/events/'+item.img">
       </div>
-    </li>
-  </ul>
+      <div class="title">{{item.title}}</div>
+      <div class="date">{{item.date}}</div>
+      <div class="details">{{item.details}}</div>
+      <div class="href">
+        <a :href="item.href" target="_blank">Visit event website -></a>
+      </div>
+    </div>
+  </div>
 </template>
-
 
 <script>
   export default {
@@ -26,69 +21,59 @@
   }
 </script>
 
-
 <style lang="stylus">
-@require '../styles/variables.styl'
-.card-event
-  .clearfix
-    padding-bottom 20px
+  @require '../styles/variables.styl'
+  .card-events
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    .flexlist
+      width: 48%;
+      padding 1vh 2vh 0vh 0vh
+      padding-top 20px
 
-  .clearfix:after
-    position relative
-    content ''
-    display: block
-    width 0
-    height 0
-    visibility hidden
-    clear both
+      .img-box
+        height 280px
+        overflow hidden
+      .img-box img
+        position relative
+        width 100%
+        top 50%
+        transform translateY(-50%)
 
-  ul
-    font-size 0
+      .title
+        padding-top 18px;
+        font-size 16px;
+        font-weight bold;
+        color txt;
 
-  li
-    padding-top 18px
-    list-style none
-    display inline-block
-    width 50%
-    text-align left
-    overflow hidden
-    vertical-align top
-    .content
-      padding 1vh
-      box-sizing border-box
-      overflow hidden
-    .img-box
-      height 30vh
-      overflow hidden
-    .img-box img
-      position relative
-      width 100%
-      top 50%
-      transform translateY(-50%)
-    .title
-      padding-top 18px
-      font-size 16px
-      font-weight bold
-      color txt
-    .date
-      color dim
-      font-weight bold
-      padding-top 2px
-      font-size 16px
-    .details
-      font-size 16px
-      font-weight normal
-      padding-top 24px
-      color light
-    .href
-      padding-top 24px
-      font-size 16px
+      .date
+        color dim;
+        font-weight bold;
+        padding-top 2px;
+        font-size 16px;
 
-  @media screen  and (min-width: 768px) and (max-width: 1024px)
-    .img-box
-      height: 22vh !important
+      .details
+        font-size 16px;
+        font-weight normal;
+        padding-top 24px;
+        color light;
+
+      .href
+        padding-top 24px;
+        font-size 16px;
+
+  @media screen  and (min-width: 768px) and (max-width: 1360px)
+    .card-events
+      .flexlist
+        .img-box
+          height 180px !important
 
   @media screen  and (max-width: 768px)
-    li
-      width 100%
+    .card-events
+      .flexlist
+        width: 100%;
+
+        .img-box
+            height 180px !important
 </style>
